70fb8da7e5f0501b56f6a4f31586f93114c2f325
Matteo <bernabeimatteo4@gmail.com>
2019-11-22 11:04:21 +0100

    now return the integer of a string number

--- src/calculator.js
+++ src/calculator.js
@@ -3,6 +3,8 @@ class Calculator {
     if (input.length > 1) {
       const reducer = (accumulator, currentValue) => accumulator + currentValue;
       return input.reduce(reducer);
+    } else if (input != "") {
+      return Number.parseInt(input);
     }
     return 0;
   }
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -2,13 +2,17 @@
 
 const { Calculator } = require("../src/calculator");
 describe("Calculator", () => {
+  const calculator = new Calculator();
+
   it("empty string should sum up to 0", () => {
-    const calculator = new Calculator();
     expect(calculator.add("")).toBe(0);
   });
 
   it("takes two numbers and return the sum between them", () => {
-    const calculator = new Calculator();
     expect(calculator.add(1, 2)).toBe(3);
+  });
+
+  it("take the number in format string and return the integer of that string", () => {
+    expect(calculator.add("1")).toBe(1);
   });
 });
